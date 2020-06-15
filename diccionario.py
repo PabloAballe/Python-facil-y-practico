3297697db91b7b009eb9c85e237c3bab3b635e64
Pablo Aballe <pablo970616@gmail.com>
2020-06-15 19:53:30 +0200

    Subo la seccion de diccionario

---
+++ diccionario.py
@@ -0,0 +1,14 @@
+#ejemplo de diccionario con Python
+
+dic={
+    'nombre': 'Pablo',
+    'apellido': 'Aballe',
+    'numero': 5,
+    'libro': 'Python fácil y práctico',
+    'habilidades': ['Python','Django', 'Tkinter']
+}
+
+
+dic.pop('libro')
+
+print(dic)
